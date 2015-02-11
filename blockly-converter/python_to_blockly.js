9fc9d954b26cb42b90b5f9db7b2b0e6ddf0ebb7e
acbart <acbart@vt.edu>
2015-02-11 16:38:52 -0500

    Organizing the repo

--- blockly-converter/python_to_blockly.js
+++ blockly-converter/python_to_blockly.js
@@ -24,6 +24,15 @@ Python2Blockly.prototype._createLogicBoolean = function(truth) {
     }
 }
 
+Python2Blockly.prototype._createNone = function() {
+    var Raw = document.createElement("block");
+    Raw.setAttribute("type", "logic_null");
+    Raw.setAttribute("inline", "false");
+    return Raw;
+}
+
+
+
 skulptToBlockly_logicOperations = function(op) {
     switch (op) {
         case "Eq": return "EQ";
@@ -34,6 +43,7 @@ skulptToBlockly_logicOperations = function(op) {
         case "GtE": return "GTE";
         case "And": return "AND";
         case "Or": return "OR";
+        // Is, IsNot, In, NotIn
         default: return "Error";
     }
 }
@@ -49,6 +59,53 @@ Python2Blockly.prototype._createLogicOperation = function(expression) {
                                    this._convertExpression("", expression.values[0]),
                                    this._convertExpression("", expression.values[1]))
 }
+Python2Blockly.prototype._createLogicNegate = function(expression) {
+    return this._createUnaryBlock("logic_negate", "false", "BOOL",
+                                  this._convertExpression("", expression.operand));
+}
+
+Python2Blockly.prototype._createList = function(expression) {
+    var Raw = document.createElement("block");
+    Raw.setAttribute("type", "lists_create_with");
+    Raw.setAttribute("inline", "false");
+    
+    var Mutation = document.createElement("mutation");
+    Mutation.setAttribute("items", expression.elts.length);
+    Raw.appendChild(Mutation);
+    
+    for (var i = 0; i < expression.elts.length; i+= 1) {
+        var Value = document.createElement("value");
+        Value.setAttribute("name", "ADD"+i);
+        Value.appendChild(this._convertExpression("", expression.elts[i]));
+        Raw.appendChild(Value);
+    }
+        
+    return Raw;
+}
+
+Python2Blockly.prototype._createPrint = function(parent, statement) {
+    var Raw = document.createElement("block");
+    Raw.setAttribute("type", "text_print_multiple");
+    Raw.setAttribute("inline", "false");
+    
+    var Mutation = document.createElement("mutation");
+    Mutation.setAttribute("items", statement.values.length);
+    Raw.appendChild(Mutation);
+    
+    if (statement.nl) {
+        // Add support for no line break (also, "dest"?)
+    }
+    
+    for (var i = 0; i < statement.values.length; i+= 1) {
+        var Value = document.createElement("value");
+        Value.setAttribute("name", "PRINT"+i);
+        Value.appendChild(this._convertExpression("", statement.values[i]));
+        Raw.appendChild(Value);
+    }
+        
+    return Raw;
+}
+
 Python2Blockly.prototype._createBinaryBlock = function(type, inline, title, choice, valueA, valueB) {
     var Raw = document.createElement("block");
     Raw.setAttribute("type", type);
@@ -64,6 +121,17 @@ Python2Blockly.prototype._createBinaryBlock = function(type, inline, title, choi
     ValueTitle = document.createElement("value");
     ValueTitle.setAttribute("name", "B");
     ValueTitle.appendChild(valueB);
+    Raw.appendChild(ValueTitle);
+    return Raw;
+}
+
+Python2Blockly.prototype._createUnaryBlock = function(type, inline, title, value) {
+    var Raw = document.createElement("block");
+    Raw.setAttribute("type", type);
+    Raw.setAttribute("inline", inline);
+    var ValueTitle = document.createElement("value");
+    ValueTitle.setAttribute("name", title);
+    ValueTitle.appendChild(value);
     Raw.appendChild(ValueTitle);
     return Raw;
 }
@@ -88,6 +156,8 @@ Python2Blockly.prototype._createVariableGet = function(body) {
 
 //<block type="math_number"> <title name="NUM">0</title> </block>
 Python2Blockly.prototype._convertAssignment = function(parent, statement) {
+    
+    // Handle expr_context
     if (statement.targets.length == 1) {
         var var_name = statement.targets[0].id.v;
         var var_value = this._convertExpression(parent, statement.value);
@@ -110,6 +180,67 @@ Python2Blockly.prototype._convertAssignment = function(parent, statement) {
     } else {
         return this._createRawBlock(statement);
     }
+}
+
+Python2Blockly.prototype._convertFor = function(parent, statement) {
+    var For_ = document.createElement("block");
+    For_.setAttribute("type", "controls_forEach");
+    For_.setAttribute("inline", "false");
+    
+    var TargetTitle = document.createElement("title");
+    TargetTitle.setAttribute("name", "VAR");
+    TargetTitle.appendChild(this._convertExpression(For_, statement.target));
+    For_.appendChild(TargetTitle);
+    
+    var IterValue = document.createElement("value");
+    IterValue.setAttribute("name", "LIST");
+    IterValue.appendChild(this._convertExpression(For_, statement.iter));
+    For_.appendChild(IterValue);
+    
+    var BodyStatement = document.createElement("statement");
+    BodyStatement.setAttribute("name", "DO");
+    this._convertBody(BodyStatement, statement.body);
+    For_.appendChild(BodyStatement);
+    
+    if (statement.orelse) {
+        // TODO: Need to have a orelse block in the For_
+    }
+    
+    return For_;
+}
+
+Python2Blockly.prototype._convertFunctionDef = function(parent, statement) {
+    var Def_ = document.createElement("block");
+    Def_.setAttribute("type", "procedures_defnoreturn");
+    Def_.setAttribute("inline", "false");
+    
+    var DefTitle = document.createElement("title");
+    DefTitle.setAttribute("name", "NAME");
+    DefTitle.appendChild(document.createTextNode(statement.name.v));
+    Def_.appendChild(DefTitle);
+    
+    if (statement.decorator_list) {
+        // TODO
+    }
+    
+    // statement.args.kwarg
+    // statement.args.vararg
+    // statement.args.defaults
+    var Mutation = document.createElement("mutation");
+    for (var i = 0; i < statement.args.args.length; i += 1) {
+        var Argument = document.createElement("arg");
+        Argument.setAttribute("name", statement.args.args[i].id.v);
+        Mutation.appendChild(Argument);
+    }
+    Def_.appendChild(Mutation);
+    
+    var BodyStatement = document.createElement("statement");
+    BodyStatement.setAttribute("name", "STACK");
+    this._convertBody(BodyStatement, statement.body);
+    Def_.appendChild(BodyStatement);
+    
+    return Def_;
+    
 }
 
 Python2Blockly.prototype._convertIf = function(parent, statement) {
@@ -174,13 +305,47 @@ Python2Blockly.prototype._convertExpression = function(parent, expression) {
             switch (expression.id.v) {
                 case "True":  return this._createLogicBoolean(true);
                 case "False": return this._createLogicBoolean(false);
-                case "None":  
+                case "None":  return this._createNone();
                 default:      return this._createVariableGet(expression);
             }
+        case "Call":
+            break; // TODO
+        case "Attribute":
+            break; // TODO
+        case "Subscript":
+            break; // TODO
+        case "Dict":
+            break; // TODO
+        case "Tuple":
+            break; // TODO
+        case "Set":
+            break; // TODO
+        case "List":
+            return this._createList(expression);
         case "Num":
             return this._createMathNumber(expression);
+        case "Str":
+            break; // TODO
         case "Compare":
             return this._createLogicCompare(expression);
+        case "UnaryOp":
+            return this._createLogicNegate(expression);
+        case "BinOp":
+            break; // TODO
+        case "Lambda":
+            break; // TODO
+        case "IfExp":
+            break; // TODO
+        case "ListComp":
+            break; // TODO
+        case "DictComp":
+            break; // TODO
+        case "GeneratorExp":
+            break; // TODO
+        case "Yield":
+            break; // TODO
+        case "Repr":
+            break; // TODO
         case "BoolOp":
             return this._createLogicOperation(expression);
     }
@@ -205,23 +370,39 @@ Main dispatch function
 Python2Blockly.prototype._convertStatement = function(parent, statement) {
     switch (statement.constructor.name) {
         case "Import_":
-            console.warn(": Everyone walk the dinosaur");
+            // TODO
+            break;
+        case "ImportFrom":
+            // TODO
             break;
         case "Expr":
             return this._convertExpression(parent, statement.value);
-        case "FunctionDef":
-            console.warn(": Function declaration.");
-            //convertBody(statement.body, 1+indent);
-            //console.warn("Body: "+statement.body);
+        case "With":
+            // TODO
             break;
+        case "While":
+            // TODO
+            break;
+        case "ClassDef":
+            // TODO
+            break;
+        case "FunctionDef":
+            return this._convertFunctionDef(parent, statement);
+        case "Print":
+            return this._createPrint(parent, statement);
         case "If_":
             return this._convertIf(parent, statement);
         case "For_":
-            console.warn(": Iteration");
-            //convertBody(statement.body, 1+indent);
+            return this._convertFor(parent, statement);
+        case "AugAssign":
+            // TODO
             break;
         case "Assign":
             return this._convertAssignment(parent, statement);
+        case "Delete":
+            // TODO
+            break;
+        // Raise, TryExcept, TryFinally, Assert, Global, Break, Continue, Pass
         default:
             console.log(": "+statement.constructor.name);
             break;
@@ -274,75 +455,22 @@ Python2Blockly.prototype.convert = function(python_source) {
     return this.XML;
 }
 
-
-/*
-Python2Blockly.prototype._convertBody = function(statements, indent) {
-    for (var i = 0; i < statements.length; i+= 1) {
-        convertStatement(statements[i], indent);
-    }
-}
-
-<block type="variables_set" inline="false" x="12" y="48">
-    <title name="VAR">item</title>
-    <value name="VALUE">
-        <block type="math_number">
-            <title name="NUM">0</title>
-        </block>
-    </value>
-    <next>
-
-function convertAssignment(assignment) {
-    if (assignments.targets.length == 1) {
-    } else {
-        for (var i = 0; i < assignment.targets.length; i+= 1) {
-            console.log("ASSIGN = "+assignment.targets[i].id.v);
-        }
-    }
-}
-
-function convertStatement(statement, indent) {
-    console.log(statement);
-    switch (statement.constructor.name) {
-        case "Import_":
-            console.warn(indent+": Everyone walk the dinosaur");
-            break;
-        case "FunctionDef":
-            console.warn(indent+": Function declaration.");
-            convertBody(statement.body, 1+indent);
-            //console.warn("Body: "+statement.body);
-            break;
-        case "If_":
-            console.warn(indent+": Conditional");
-            convertBody(statement.body, 1+indent);
-            break;
-        case "For_":
-            console.warn(indent+": Iteration");
-            convertBody(statement.body, 1+indent);
-            break;
-        case "Assign":
-            convertAssignment(statement);
-        default:
-            console.log(indent+": "+statement.constructor.name);
-            break;
-    }
-}
-
-function convert_code(python_source) {
-    console.log("-------------------");
+Python2Blockly.prototype.convertToRaw = function(python_source) {
     var filename = 'user_code.py';
     var parse_tree, symbol_table, error_message;
     try {
-        parse_tree = Sk.astFromParse(Sk.parse(filename, code), filename);
+        parse_tree = Sk.astFromParse(Sk.parse(filename, python_source), filename);
         symbol_table = Sk.symboltable(parse_tree, filename);
         error_message = false;
     } catch (e) {
         error_message = e;
     }
+    this.sourceCodeLines = python_source.split("\n");
     
     if (error_message !== false) {
         console.log("Error: "+error_message);
     } else {
-        convertBody(parse_tree.body, 0);
+        this.XML.appendChild(this._createRawBlock(python_source));
     }
+    return this.XML;
 }
-*/
