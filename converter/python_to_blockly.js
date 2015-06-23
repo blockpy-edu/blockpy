851e452f95abf11d9250e856c0d29568cbc169ab
acbart <acbart@vt.edu>
2015-06-23 16:20:26 -0400

    Much refactoring

--- converter/python_to_blockly.js
+++ converter/python_to_blockly.js
@@ -25,27 +25,32 @@
 'use strict';
 
 /**
+ *
+ */
+function PythonAST() {
+};
+
+PythonAST.prototype.AugAssign = function() {
+};
+
+/**
  * @constructor 
  */
-function Python2Blockly() {
+function PythonToBlocks() {
     this.XML = document.createElement("xml");
     this.XML.setAttribute("xmlns", "http://www.w3.org/1999/xhtml");
     
     this.current = this.XML;
     this.parent = [];
-    
-    //Node.appendChild( document.createElement("testingOne") );
-    //Node.appendChild( document.createElement("TestingTwo") );
-    //Node.appendChild( document.createElement("TestingThree") );
 };
 
-Python2Blockly.prototype._createRawBlock = function(body) {
+PythonToBlocks.prototype._createRawBlock = function(body) {
     return this._createSimpleBlock("raw_block", "TEXT", body.$r().v);
 }
-Python2Blockly.prototype._createRawExpression = function(body) {
+PythonToBlocks.prototype._createRawExpression = function(body) {
     return this._createSimpleBlock("raw_expression", "TEXT", body.$r().v);
 }
-Python2Blockly.prototype._createLogicBoolean = function(truth) {
+PythonToBlocks.prototype._createLogicBoolean = function(truth) {
     if (truth) {
         return this._createSimpleBlock("logic_boolean", "BOOL", "TRUE");
     } else {
@@ -53,7 +58,7 @@ Python2Blockly.prototype._createLogicBoolean = function(truth) {
     }
 }
 
-Python2Blockly.prototype._createString = function(value) {
+PythonToBlocks.prototype._createString = function(value) {
     var Raw = document.createElement("block");
     Raw.setAttribute("type", "text");
     var Field = document.createElement("field");
@@ -63,7 +68,7 @@ Python2Blockly.prototype._createString = function(value) {
     return Raw;
 }
 
-Python2Blockly.prototype._createNone = function() {
+PythonToBlocks.prototype._createNone = function() {
     var Raw = document.createElement("block");
     Raw.setAttribute("type", "logic_null");
     Raw.setAttribute("inline", "false");
@@ -98,25 +103,25 @@ var skulptToBlockly_mathArithmetic = function(op) {
     }
 }
 
-Python2Blockly.prototype.convertAugAssign_ = function(expression) {
+PythonToBlocks.prototype.convertAugAssign_ = function(expression) {
     //math_change
     // TODO
     throw "Augmented Assignment is not currently supported.";
 }
 
-Python2Blockly.prototype._createLogicCompare = function(expression) {
+PythonToBlocks.prototype._createLogicCompare = function(expression) {
     return this._createBinaryBlock("logic_compare", "true", "OP",
                                    skulptToBlockly_logicOperations(expression.ops[0].name),
                                    this._convertExpression("", expression.left),
                                    this._convertExpression("", expression.comparators[0]))
 }
-Python2Blockly.prototype._createBinOp = function(expression) {
+PythonToBlocks.prototype._createBinOp = function(expression) {
     return this._createBinaryBlock("math_arithmetic", "true", "OP",
                                    skulptToBlockly_mathArithmetic(expression.op.name),
                                    this._convertExpression("", expression.left),
                                    this._convertExpression("", expression.right))
 }
-Python2Blockly.prototype.createAppendBlock_ = function(func, args) {
+PythonToBlocks.prototype.createAppendBlock_ = function(func, args) {
     var Block_ = document.createElement("block");
     Block_.setAttribute("type", "lists_append");
     Block_.setAttribute("inline", "false");
@@ -134,7 +139,7 @@ Python2Blockly.prototype.createAppendBlock_ = function(func, args) {
     Block_.appendChild(ListValue);
     return Block_;
 }
-Python2Blockly.prototype.createDictSubscript_ = function(expression) {
+PythonToBlocks.prototype.createDictSubscript_ = function(expression) {
     var Raw = document.createElement("block");
     Raw.setAttribute("type", "dict_get_literal");
     Raw.setAttribute("inline", "false");
@@ -154,18 +159,18 @@ Python2Blockly.prototype.createDictSubscript_ = function(expression) {
     Raw.appendChild(DictValue);
     return Raw;
 }
-Python2Blockly.prototype._createLogicOperation = function(expression) {
+PythonToBlocks.prototype._createLogicOperation = function(expression) {
     return this._createBinaryBlock("logic_operation", "true", "OP",
                                    skulptToBlockly_logicOperations(expression.op.name),
                                    this._convertExpression("", expression.values[0]),
                                    this._convertExpression("", expression.values[1]))
 }
-Python2Blockly.prototype._createLogicNegate = function(expression) {
+PythonToBlocks.prototype._createLogicNegate = function(expression) {
     return this._createUnaryBlock("logic_negate", "false", "BOOL",
                                   this._convertExpression("", expression.operand));
 }
 
-Python2Blockly.prototype._createBuiltinBlock = function(type, fields) {
+PythonToBlocks.prototype._createBuiltinBlock = function(type, fields) {
     var Raw = document.createElement("block");
     Raw.setAttribute("type", type);
     Raw.setAttribute("inline", "true");
@@ -181,7 +186,7 @@ Python2Blockly.prototype._createBuiltinBlock = function(type, fields) {
     return Raw;
 }
 
-Python2Blockly.prototype._createPlot = function(expression) {
+PythonToBlocks.prototype._createPlot = function(expression) {
     var Raw = document.createElement("block");
     Raw.setAttribute("type", "plot_line");
     Raw.setAttribute("inline", "false");
@@ -194,7 +199,7 @@ Python2Blockly.prototype._createPlot = function(expression) {
     return Raw;
 }
 
-Python2Blockly.prototype._createCallBlock = function(expression) {
+PythonToBlocks.prototype._createCallBlock = function(expression) {
     var Raw = document.createElement("block");
     Raw.setAttribute("type", "procedures_callreturn");
     Raw.setAttribute("inline", "false");
@@ -220,7 +225,7 @@ Python2Blockly.prototype._createCallBlock = function(expression) {
     return Raw;
 }
 
-Python2Blockly.prototype.createDict_ = function(expression) {
+PythonToBlocks.prototype.createDict_ = function(expression) {
     var Raw = document.createElement("block");
     Raw.setAttribute("type", "dicts_create_with");
     Raw.setAttribute("inline", "false");
@@ -243,7 +248,7 @@ Python2Blockly.prototype.createDict_ = function(expression) {
     return Raw;
 }
 
-Python2Blockly.prototype._createList = function(expression) {
+PythonToBlocks.prototype._createList = function(expression) {
     var Raw = document.createElement("block");
     Raw.setAttribute("type", "lists_create_with");
     Raw.setAttribute("inline", "false");
@@ -262,7 +267,7 @@ Python2Blockly.prototype._createList = function(expression) {
     return Raw;
 }
 
-Python2Blockly.prototype.convertReturn_ = function(parent, statement) {
+PythonToBlocks.prototype.convertReturn_ = function(parent, statement) {
     var Block_ = document.createElement("block");
     Block_.setAttribute("type", "procedures_return");
     Block_.setAttribute("inline", "false");
@@ -276,7 +281,7 @@ Python2Blockly.prototype.convertReturn_ = function(parent, statement) {
     return Block_;
 }
 
-Python2Blockly.prototype._createPrint = function(parent, statement) {
+PythonToBlocks.prototype._createPrint = function(parent, statement) {
     var Raw = document.createElement("block");
     Raw.setAttribute("type", "text_print_multiple");
     Raw.setAttribute("inline", "false");
@@ -299,7 +304,7 @@ Python2Blockly.prototype._createPrint = function(parent, statement) {
     return Raw;
 }
 
-Python2Blockly.prototype._createBinaryBlock = function(type, inline, title, choice, valueA, valueB) {
+PythonToBlocks.prototype._createBinaryBlock = function(type, inline, title, choice, valueA, valueB) {
     var Raw = document.createElement("block");
     Raw.setAttribute("type", type);
     Raw.setAttribute("inline", inline);
@@ -318,7 +323,7 @@ Python2Blockly.prototype._createBinaryBlock = function(type, inline, title, choi
     return Raw;
 }
 
-Python2Blockly.prototype._createUnaryBlock = function(type, inline, title, value) {
+PythonToBlocks.prototype._createUnaryBlock = function(type, inline, title, value) {
     var Raw = document.createElement("block");
     Raw.setAttribute("type", type);
     Raw.setAttribute("inline", inline);
@@ -329,7 +334,7 @@ Python2Blockly.prototype._createUnaryBlock = function(type, inline, title, value
     return Raw;
 }
 
-Python2Blockly.prototype._createSimpleBlock = function(type, name, value) {
+PythonToBlocks.prototype._createSimpleBlock = function(type, name, value) {
     var Raw = document.createElement("block");
     Raw.setAttribute("type", type);
     var Title = document.createElement("title");
@@ -339,16 +344,16 @@ Python2Blockly.prototype._createSimpleBlock = function(type, name, value) {
     return Raw;
 }
 
-Python2Blockly.prototype._createMathNumber = function(body) {
+PythonToBlocks.prototype._createMathNumber = function(body) {
     return this._createSimpleBlock("math_number", "NUM", body.n.v);
 }
 
-Python2Blockly.prototype._createVariableGet = function(body) {
+PythonToBlocks.prototype._createVariableGet = function(body) {
     return this._createSimpleBlock("variables_get", "VAR", body.id.v);
 }
 
 //<block type="math_number"> <title name="NUM">0</title> </block>
-Python2Blockly.prototype._convertAssignment = function(parent, statement) {
+PythonToBlocks.prototype._convertAssignment = function(parent, statement) {
     
     // Handle expr_context
     if (statement.targets.length == 1) {
@@ -375,7 +380,7 @@ Python2Blockly.prototype._convertAssignment = function(parent, statement) {
     }
 }
 
-Python2Blockly.prototype._convertFor = function(parent, statement) {
+PythonToBlocks.prototype._convertFor = function(parent, statement) {
     var For_ = document.createElement("block");
     For_.setAttribute("type", "controls_forEach");
     For_.setAttribute("inline", "false");
@@ -402,7 +407,7 @@ Python2Blockly.prototype._convertFor = function(parent, statement) {
     return For_;
 }
 
-Python2Blockly.prototype._convertFunctionDef = function(parent, statement) {
+PythonToBlocks.prototype._convertFunctionDef = function(parent, statement) {
     var Def_ = document.createElement("block");
     Def_.setAttribute("type", "procedures_defnoreturn");
     Def_.setAttribute("inline", "false");
@@ -436,7 +441,7 @@ Python2Blockly.prototype._convertFunctionDef = function(parent, statement) {
     
 }
 
-Python2Blockly.prototype._convertIf = function(parent, statement) {
+PythonToBlocks.prototype._convertIf = function(parent, statement) {
     var If_ = document.createElement("block");
     If_.setAttribute("type", "controls_if");
     If_.setAttribute("inline", "false");
@@ -504,7 +509,7 @@ function toTitleCase(str)
     return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
 }
 
-Python2Blockly.prototype._convertExpression = function(parent, expression) {
+PythonToBlocks.prototype._convertExpression = function(parent, expression) {
     switch (expression.constructor.name) {
         case "Name":
             switch (expression.id.v) {
@@ -637,7 +642,7 @@ Python2Blockly.prototype._convertExpression = function(parent, expression) {
     return this._createRawExpression(expression);
 }
 
-Python2Blockly.prototype._hasNextStatement = function(statement) {
+PythonToBlocks.prototype._hasNextStatement = function(statement) {
     switch (statement.constructor.name) {
         case "Import_": return true;
         case "Expr":
@@ -661,7 +666,7 @@ Python2Blockly.prototype._hasNextStatement = function(statement) {
 /*
 Main dispatch function
 */
-Python2Blockly.prototype._convertStatement = function(parent, statement) {
+PythonToBlocks.prototype._convertStatement = function(parent, statement) {
     switch (statement.constructor.name) {
         case "Import_":
             return null;
@@ -699,7 +704,7 @@ Python2Blockly.prototype._convertStatement = function(parent, statement) {
     return this._createRawBlock(statement);//this.sourceCodeLines[statement.lineno-1]);
 }
 
-Python2Blockly.prototype._convertBody = function(parent, statements) {
+PythonToBlocks.prototype._convertBody = function(parent, statements) {
     var current = parent;
     for (var i = 0; i < statements.length; i+= 1) {
         if (statements[i].constructor.name != "Pass") {
@@ -726,7 +731,7 @@ Python2Blockly.prototype._convertBody = function(parent, statements) {
     }
 }
 
-Python2Blockly.prototype.convert = function(python_source) {
+PythonToBlocks.prototype.convert = function(python_source) {
     var filename = 'user_code.py';
     var parse_tree, symbol_table, error_message;
     try {
@@ -747,7 +752,7 @@ Python2Blockly.prototype.convert = function(python_source) {
     return this.XML;
 }
 
-Python2Blockly.prototype.convertToRaw = function(python_source) {
+PythonToBlocks.prototype.convertToRaw = function(python_source) {
     var filename = 'user_code.py';
     var parse_tree, symbol_table, error_message;
     try {
