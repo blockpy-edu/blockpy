cd997d6939e92b7cb4dc0c3e34980aae046bb01f
Austin Cory Bart <acbart@vt.edu>
2015-10-17 16:35:57 -0400

    Fixed gitignores

---
+++ server/controllers/assets.py
@@ -0,0 +1,44 @@
+from flask.ext.assets import Bundle, Environment
+from main import app
+
+bundles = {
+
+    'blockpy_js': Bundle(
+        'libs/jquery.hotkeys.js',
+        'libs/d3.min.js',
+        'libs/math.0.19.0.min.js',
+        'libs/bootstrap-wysiwyg.js',
+        "libs/mindmup-editabletable.js",
+        "libs/codemirror/codemirror.js",
+        "libs/codemirror/python.js",
+        "libs/codemirror/htmlmixed.js",
+        "libs/codemirror/xml.js",
+        "libs/summernote/summernote.min.js",
+        "libs/summernote/summernote-ext-hint.js",
+        "libs/summernote/summernote-ext-video.js",
+        "libs/crime_data.js",
+        "blockly/blockly_compressed.js",
+        "blockly/blocks_compressed.js",
+        "blockly/python_compressed.js",
+        "blockly/javascript_compressed.js",
+        "blockly/msg/js/en.js",
+        "skulpt/dist/skulpt.min.js",
+        "skulpt/dist/skulpt-stdlib.js",
+        "analyzer/analyzer.js",
+        "analyzer/python_errors.js",
+        "converter/python_to_blockly.js",
+        "kennel/kennel.js",
+        "converter/renderBlocklyToPng.js",
+        output='gen/blockpy.js'),
+
+    'blockpy_css': Bundle(
+        'libs/codemirror/codemirror.css',
+        'libs/font-awesome.min.css',
+        'libs/summernote/summernote.css',
+        'kennel/kennel.css',
+        output='gen/blockpy.css'),
+}
+
+assets = Environment(app)
+
+assets.register(bundles)
