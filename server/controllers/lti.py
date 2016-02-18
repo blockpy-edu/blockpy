abe0f6f4ed3218d03157d14c054b3694a2ccc19a
Austin Cory Bart <acbart@vt.edu>
2016-02-18 00:22:14 -0500

    Posting grade fix

--- server/controllers/lti.py
+++ server/controllers/lti.py
@@ -218,10 +218,12 @@ def save_correct(lti=lti):
     else:
         message = "Incomplete"
     url = url_for('lti_assignments.get_submission_code', submission_id=submission.id, _external=True)
+    if 'lis_result_sourcedid' not in session:
+        return jsonify(success=False, message="Not in a grading context.")
     if assignment.mode == 'maze':
         lti.post_grade(float(submission.correct), "<h1>{0}</h1>".format(message));
     else:
-        lti.post_grade(float(submission.correct), "<h1>{0}</h1>".format(message)+"<div>Latest work in progress: <a href='{0}' target='_blank'>View</a></div>"+"<div>Touches: {0}</div>".format(submission.version)+"Last ran code:<br>".format(url)+highlight(submission.code, PythonLexer(), HtmlFormatter()))
+        lti.post_grade(float(submission.correct), "<h1>{0}</h1>".format(message)+"<div>Latest work in progress: <a href='{0}' target='_blank'>View</a></div>".format(url)+"<div>Touches: {0}</div>".format(submission.version)+"Last ran code:<br>"+highlight(submission.code, PythonLexer(), HtmlFormatter()))
     return jsonify(success=True)
     
 @lti_assignments.route('/get_submission_code/', methods=['GET', 'POST'])
