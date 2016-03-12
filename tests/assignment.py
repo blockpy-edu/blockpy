b1dfc4c80d9dbd8fecd89559814bd6a80717f9ee
Austin Cory Bart <acbart@vt.edu>
2016-03-12 11:44:18 -0500

    Assessment data

---
+++ tests/assignment.py
@@ -0,0 +1,27 @@
+import weather
+
+___ = 5
+
+weather.get_temperature("Miami, FL")
+
+32
+
+print(___)
+
+fahrenheit = ___
+
+print(___)
+
+___ - ___
+
+celsius = ___
+
+fahrenheit
+
+1.8
+
+___ / ___
+
+fahrenheit
+
+celsius
