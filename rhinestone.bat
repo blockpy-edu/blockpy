f8f10128f5e0ad5a31a035675d2edb0691a7e35e
Austin Cory Bart <acbart@vt.edu>
2016-08-26 22:03:50 -0400

    Clear out old files

--- rhinestone.bat
+++
@@ -1,8 +0,0 @@
-copy C:\Users\acbart\Projects\blockpy\blockly\blockly_compressed.js C:\Users\acbart\Projects\runestone\directives\blockly\js\blockly_compressed.js
-copy C:\Users\acbart\Projects\blockpy\blockly\blockly_uncompressed.js C:\Users\acbart\Projects\runestone\directives\blockly\js\blockly_uncompressed.js
-copy C:\Users\acbart\Projects\blockpy\blockly\blocks_compressed.js C:\Users\acbart\Projects\runestone\directives\blockly\js\blocks_compressed.js
-copy C:\Users\acbart\Projects\blockpy\blockly\python_compressed.js C:\Users\acbart\Projects\runestone\directives\blockly\js\python_compressed.js
-copy C:\Users\acbart\Projects\blockpy\blockly\javascript_compressed.js C:\Users\acbart\Projects\runestone\directives\blockly\js\javascript_compressed.js
-
-copy C:\Users\acbart\Projects\blockpy\kennel_dist\blockpy.js C:\Users\acbart\Projects\runestone\directives\blockly\js\blockpy.js
-copy C:\Users\acbart\Projects\blockpy\kennel_dist\blockpy.css C:\Users\acbart\Projects\runestone\directives\blockly\js\blockpy.css
