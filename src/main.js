1c36305ba3a23603179b4818877bd6e269c656d1
Austin Cory Bart <acbart@vt.edu>
2016-11-23 01:08:33 -0500

    Documenting the Main, Toolbar, and Dialog files

--- src/main.js
+++ src/main.js
@@ -1,3 +1,15 @@
+/**
+ * A helper function for extending an array based
+ * on an "addArray" and "removeArray". Any element
+ * found in removeArray is removed from the first array
+ * and all the elements of addArray are added.
+ * Creates a new array, so is non-destructive.
+ *
+ * @param {Array} array - the array to manipulate
+ * @param {Array} addArray - the elements to add to the array
+ * @param {Array} removeArray - the elements to remove from the array
+ * @return {Array} The modified array
+ */
 function expandArray(array, addArray, removeArray) {
     var copyArray = array.filter(function(item) {
         return removeArray.indexOf(item) === -1;
@@ -5,6 +17,16 @@ function expandArray(array, addArray, removeArray) {
     return copyArray.concat(addArray);
 }
 
+/**
+ * Creates an instance of BlockPy
+ *
+ * @constructor
+ * @this {BlockPy}
+ * @param {Object} settings - User level settings (e.g., what view mode, whether to mute semantic errors, etc.)
+ * @param {Object} assignment - Assignment level settings (data about the loaded assignment, user, submission, etc.)
+ * @param {Object} submission - Unused parameter.
+ * @param {Object} programs - Includes the source code of any programs to be loaded
+ */
 function BlockPy(settings, assignment, submission, programs) {
     this.model = {
         // User level settings
@@ -97,14 +119,18 @@ function BlockPy(settings, assignment, submission, programs) {
             "give_feedback": ko.observable(assignment.give_feedback),
         }
     };
+    
+    // The code for the current active program file (e.g., "__main__")
     this.model.program = ko.computed(function() {
         return this.programs[this.settings.filename()]();
     }, this.model) //.extend({ rateLimit: { method: "notifyWhenChangesStop", timeout: 400 } });
     
+    // Whether this URL has been specified
     this.model.server_is_connected = function(url) {
         return this.constants.urls !== undefined && this.constants.urls[url] !== undefined;
     };
     
+    // Helper function to map error statuses to UI elements
     this.model.status_feedback_class = ko.computed(function() {
         switch (this.status.error()) {
             default: case 'none': return ['label-none', ''];
@@ -119,6 +145,7 @@ function BlockPy(settings, assignment, submission, programs) {
         }
     }, this.model);
     
+    // Helper function to map Server error statuses to UI elements
     this.model.status_server_class = ko.computed(function() {
         switch (this.status.server()) {
             default: case 'Loading': return ['label-default', 'Loading'];
@@ -132,6 +159,7 @@ function BlockPy(settings, assignment, submission, programs) {
         }
     }, this.model);
     
+    // Program trace functions
     var execution = this.model.execution;
     this.model.moveTraceFirst = function(index) { 
         execution.trace_step(0); };
@@ -147,6 +175,14 @@ function BlockPy(settings, assignment, submission, programs) {
         return execution.trace()[Math.min(execution.trace().length-1, execution.trace_step())];
     });
     
+    /**
+     * Opens a new window to represent the exact value of a Skulpt object.
+     * Particularly useful for things like lists that can be really, really
+     * long.
+     * 
+     * @param {String} type - The type of the value
+     * @param {Object} exact_value - A Skulpt value to be rendered.
+     */
     this.model.viewExactValue = function(type, exact_value) {
         return function() {
             if (type == "List") {
@@ -158,6 +194,7 @@ function BlockPy(settings, assignment, submission, programs) {
     }
     
     // For performance reasons, batch notifications for execution handling.
+    // I'm not even sure these have any value any more.
     execution.trace.extend({ rateLimit: { timeout: 20, method: "notifyWhenChangesStop" } });
     execution.step.extend({ rateLimit: { timeout: 20, method: "notifyWhenChangesStop" } });
     execution.last_step.extend({ rateLimit: { timeout: 20, method: "notifyWhenChangesStop" } });
@@ -166,12 +203,22 @@ function BlockPy(settings, assignment, submission, programs) {
     this.initMain();
 }
 
+/**
+ * The default modules to make available to the user.
+ *
+ * @type Array.<String>
+ */
 BlockPy.DEFAULT_MODULES = ['Properties', 'Decisions', 
                            'Iteration',
                            'Calculation', 'Output', 
                            'Values', 
                            'Lists', 'Dictionaries']
 
+/**
+ * Initializes the BlockPy object by initializing its interface,
+ * model, and components.
+ *
+ */
 BlockPy.prototype.initMain = function() {
     this.initInterface();
     this.initModel();
@@ -181,20 +228,39 @@ BlockPy.prototype.initMain = function() {
     }
 }
 
-BlockPy.prototype.initInterface = function(postCompletion) {
+/**
+ * Initializes the User Inteface for the instance, by loading in the
+ * HTML file (which has been manually encoded into a JS string using
+ * the build.py script). We do this because its a giant hassle to keep
+ * HTML correct when it's stored in JS strings. We should look into
+ * more sophisticated templating features, probably.
+ *
+ */
+BlockPy.prototype.initInterface = function() {
     var constants = this.model.constants;
+    // Refer to interface.js, interface.html, and build.py
     constants.container = $(constants.attachmentPoint).html($(BlockPyInterface))
 }
 
+/**
+ * Applys the KnockoutJS bindings to the model, instantiating the values into the
+ * HTML.
+ */
 BlockPy.prototype.initModel = function() {
     ko.applyBindings(this.model);
 }
 
+/**
+ * Initializes each of the relevant components of BlockPy. For more information,
+ * consult each of the component's relevant JS file in turn.
+ */
 BlockPy.prototype.initComponents = function() {
     var container = this.model.constants.container;
     this.components = {};
     var main = this,
         components = this.components;
+    // Each of these components will take the BlockPy instance, and possibly a
+    // reference to the relevant HTML location where it will be embedded.
     components.dialog = new BlockPyDialog(main, container.find('.blockpy-popup'));
     components.toolbar  = new BlockPyToolbar(main,  container.find('.blockpy-toolbar'));
     components.feedback = new BlockPyFeedback(main, container.find('.blockpy-feedback'));
@@ -209,12 +275,20 @@ BlockPy.prototype.initComponents = function() {
     main.model.status.server('Loaded')
 }
 
+/**
+ * Initiailizes certain development data, useful for testing out new modules in
+ * Skulpt.
+ */
 BlockPy.prototype.initDevelopment = function () {
     /*$.get('src/skulpt_ast.js', function(data) {
         Sk.builtinFiles['files']['src/lib/ast/__init__.js'] = data;
     });*/
 }
 
+/**
+ * Redundant method for reporting an error. If this is used anywhere, it should be
+ * converted to direct calls to components.feedback.
+ */
 BlockPy.prototype.reportError = function(component, original, message, line) {
     if (component == 'editor') {
         this.components.feedback.editorError(original, message, line);
@@ -224,151 +298,13 @@ BlockPy.prototype.reportError = function(component, original, message, line) {
     console.error(component, message)
 }
 
-/*
-function BlockPy(attachmentPoint, mode, presentation, current_code,
-                on_run, on_change, starting_code, instructor, view, blocklyPath,
-                settings,
-                urls, questionProperties) {
-    // User programs
-}
-*/
-
-BlockPy.prototype.activateToolbar = function() {
-    var elements = this.toolbar.elements;
-    var blockpy = this, server = this.server;
-    // Editor mode
-    
-    elements.to_pseudo.click(function(ev) {
-        ev.preventDefault();
-        blockpy.editor.updateBlocks();
-        server.logEvent('editor', 'pseudo');
-        var popup = blockpy.mainDiv.find('.blockpy-popup');
-        popup.find('.modal-title').html("Pseudo-code Explanation");
-        popup.find('.modal-body').html(Blockly.Pseudo.workspaceToCode(blockpy.editor.blockly));
-        popup.modal('show');
-    });
-    blockpy.mainDiv.find('.blockpy-popup').on('hidden.bs.modal', function () {
-        server.logEvent('editor', 'close_pseudo');
-    });
-    if (!this.model.settings.instructor) {
-        elements.blockpy_mode.hide();
-        //elements.to_rst.hide();
-    }
-    // Run
-    elements.run.click(function() {
-        server.logEvent('editor', 'run');
-        blockpy.run();
-    });
-    // Undo
-    elements.undo.click(function() {
-        server.logEvent('editor', 'undo');
-        if (blockpy.model.settings.editor() == 'blocks') {
-            blockpy.editor.blockly.undo();
-        } else {
-            blockpy.editor.text.undo();
-        }
-    });
-    // Redo
-    elements.redo.click(function() {
-        server.logEvent('editor', 'redo');
-        if (blockpy.model.settings.editor() == 'blocks') {
-            blockpy.editor.blockly.redo();
-        } else {
-            blockpy.editor.text.redo();
-        }
-    });
-    // Wide
-    var left = blockpy.mainDiv.find('.blockpy-content-left'),
-        right = blockpy.mainDiv.find('.blockpy-content-right');
-    elements.wide.click(function() {
-        if (elements.wide.attr("data-side") == "skinny") {
-            server.logEvent('editor', 'wide');
-            // Make it skinny
-            elements.wide.attr("data-side", "wide")
-                         .html('<i class="fa fa-ellipsis-h"></i> Wide');
-            // Left side
-            left.removeClass('col-md-10 col-sm-12 col-xs-12 col-md-offset-1');
-            left.addClass('col-md-7 col-sm-7 col-xs-7');
-            // Right side
-            right.removeClass('col-md-10 col-sm-12 col-xs-12 col-md-offset-1');
-            right.addClass('col-md-5 col-xs-5 col-sm-5');
-        } else {
-            server.logEvent('editor', 'skinny');
-            // Make it wide
-            elements.wide.attr("data-side", "skinny")
-                         .html('<i class="fa fa-ellipsis-v"></i> Skinny');
-            // Left side
-            left.removeClass('col-md-7 col-xs-7 col-sm-7');
-            left.addClass('col-md-10 col-sm-12 col-xs-12 col-md-offset-1');
-            // Right side
-            right.removeClass('col-md-5 col-xs-5 col-sm-7');
-            right.addClass('col-md-10 col-sm-12 col-xs-12 col-md-offset-1');
-        }
-        // Hack: Force the blockly window to fit the width
-        if (blockpy.model.settings.editor == 'blocks') {
-            blockpy.editor.blockly.render();
-        }
-    });
-    // Reset code
-    elements.reset.click(function() {
-        server.logEvent('editor', 'reset');
-        blockpy.setCode(blockpy.model.programs.starting_code);
-    });
-    // Clear code
-    elements.clear.click(function() {
-        server.logEvent('editor', 'clear');
-        blockpy.setCode("");
-    });
-    // Align blocks
-    elements.align.click(function() {
-        server.logEvent('editor', 'align');
-        blockpy.editor.blockly.align();
-    });
-    // Change programs
-    for (var name in this.model.programs) {
-        this.toolbar.addProgram(name);
-    }
-    this.toolbar.elements.programs.button('toggle').change(function(event) {
-        if (blockpy.silentChange_) {
-            blockpy.silentChange_ = false;
-        } else {
-            var name = $(event.target).attr("data-name");
-            blockpy.changeProgram(name);
-        }
-    });
-    
-    var parsonBox = blockpy.mainDiv.find('.blockpy-presentation-parsons-check input');
-    var textFirstBox = blockpy.mainDiv.find('.blockpy-presentation-text-first input');
-    var nameEditor = blockpy.mainDiv.find('.blockpy-presentation-name-editor');
-    var updatePresentation = function() {
-        blockpy.model.settings.parsons = parsonBox.prop('checked');
-        blockpy.model.settings.text_first = textFirstBox.prop('checked');
-        blockpy.server.savePresentation(blockpy.presentation.get(), 
-                                       nameEditor.val(), 
-                                       blockpy.model.settings.parsons,
-                                       blockpy.model.settings.text_first);
-    }
-    // Save name editing
-    nameEditor.change(updatePresentation);
-    // Parsons checkbox
-    parsonBox.change(updatePresentation).prop('checked', this.model.settings.parsons);
-    // Text first checkbox
-    textFirstBox.change(updatePresentation).prop('checked', this.model.settings.text_first);
-}
-
+/**
+ * Helper function for setting the current code. I'm not sure if this is used anywhere.
+ *
+ */
 BlockPy.prototype.setCode = function(code, name) {
     if (name === undefined) {
         name = this.model.settings.filename();
     }
     this.model.programs[name](code);
 }
-
-BlockPy.prototype.alert = function(message) {
-    var box = this.mainDiv.find('.blockpy-alert');
-    box.text(message).show();
-    return box;
-}
-
-/*
- * Resets skulpt to some default values
- */
