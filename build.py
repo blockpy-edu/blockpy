162b1029362f06262d5b76099a235305f975c219
Austin Cory Bart <acbart@vt.edu>
2017-03-26 09:45:38 -0400

    Adding docs, removing bad old code

--- build.py
+++ build.py
@@ -2,6 +2,17 @@ with open('src/interface.html', 'r') as input, open('src/interface.js', 'w') as 
     contents = input.read()
     cleaned_contents = contents.replace('"', '\\"').replace('\n', '')
     js_contents = '''
+/**
+ * An automatically generated file, based on interface.html.
+ * An interesting problem in web development is managing HTML
+ * code in JS files. Rather than embedding string literals and
+ * concatenating them, or some other hackish soluion,
+ * we simply convert an HTML file to a JS string. Therefore,
+ * relevant edits should be in interface.html instead.
+ *
+ * The BlockPyInterface global can be seen as a constant
+ * representation of the default interface.
+ */
 BlockPyInterface = "{interface_code}";
 '''.format(interface_code=cleaned_contents)
     output.write(js_contents)
--- src/english.js
+++ src/english.js
@@ -1,9 +1,18 @@
+/**
+ * An object for handling the conversion of Python to an English transliteration.
+ * Currently dummied out, but we will bring this feature back.
+ *
+ * @constructor
+ * @this {BlockPyEnglish}
+ * @param {Object} main - The main BlockPy instance
+ */
 function BlockPyEnglish(main) {
-    this.main = main;
-    
-    
+    this.main = main; 
 }
 
+/**
+ * A method for opening a dialog with the english transliteration.
+ */
 BlockPyEnglish.prototype.openDialog = function() {
     try {
         body = Blockly.Pseudo.workspaceToCode();
