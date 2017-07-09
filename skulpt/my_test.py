4179f19a3dcd442ed1d709b1394e71e73887e413
Austin Cory Bart <acbart@vt.edu>
2017-07-08 22:50:35 -0400

    Some tests

--- skulpt/my_test.py
+++ skulpt/my_test.py
@@ -1,10 +1,8 @@
-class Stack():
-    def __init__(self):
-        self.value = 5
-    def __str__(self):
-        return 'test'
-    def __repr__(self):
-        return 'alpha'
-        
-s = Stack()
-s.other_value = 10
+class TestClass():
+    def __getattr__(self, key):
+        if key == 't':
+            return 5
+        raise AttributeError(item)
+t = TestClass()
+print(t.t)
+print(t.x)
