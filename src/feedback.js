f7c6de639cb63f76f1b0829d8a8ccb95f935d564
lukesg08 <lukesg08@vt.edu>
2017-07-19 17:22:26 -0400

    Fixed bug in suppress function (sk_mod_instructor) Updating replay code for newer type of feedback testing (replay.html) updated feedback code for suppressing specific AI errors (feedback.js)

--- src/feedback.js
+++ src/feedback.js
@@ -300,7 +300,7 @@ BlockPyFeedback.prototype.presentFeedback = function() {
         return 'parser';
     }
     // Analyzer
-    if (!suppress['analyzer']) {
+    if (suppress['analyzer'] !== true) {//if a subtype is specified, or no suppression requested, present feedback
         if (!report['analyzer'].success) {
             this.internalError(report['analyzer'].error, "Analyzer Error", "Error in analyzer. Please show the above message to an instructor!");
         }
@@ -344,40 +344,39 @@ BlockPyFeedback.prototype.presentAnalyzerFeedback = function() {
     if (suppress === true) {
         // Suppress all types of analyzer errors
         return false;
-    } else if (report["Unconnected blocks"].length >= 1) {
+    } else if (!suppress["Unconnected blocks"] && report["Unconnected blocks"].length >= 1) {
         var variable = report['Unconnected blocks'][0];
         this.semanticError("Unconnected blocks", "It looks like you have unconnected blocks on line "+variable.position.line+". Before you run your program, you must make sure that all of your blocks are connected and that there are no unfilled holes.", variable.position.line)
         return true;
-    } else if (report['Iteration variable is iteration list'].length >= 1) {
+    } else if (!suppress['Iteration variable is iteration list'] && report['Iteration variable is iteration list'].length >= 1) {
         var variable = report['Iteration variable is iteration list'][0];
         this.semanticError("Iteration Problem", "The property <code>"+variable.name+"</code> was iterated on line "+variable.position.line+", but you used the same variable as the iteration variable. You should choose a different variable name for the iteration variable. Usually, the iteration variable is the singular form of the iteration list (e.g., <code>for dog in dogs:</code>).", variable.position.line)
         return true;
-    } else if (report["Undefined variables"].length >= 1) {
+    } else if (!suppress["Undefined variables"] && report["Undefined variables"].length >= 1) {
         var variable = report["Undefined variables"][0];
         this.semanticError("Initialization Problem", "The property <code>"+variable.name+"</code> was read on line "+variable.position.line+", but it was not given a value on a previous line. You cannot use a property until it has been initialized.", variable.position.line)
         return true;
-    } else if (report["Possibly undefined variables"].length >= 1) {
+    } else if (!suppress["Possibly undefined variables"] && report["Possibly undefined variables"].length >= 1) {
         var variable = report["Possibly undefined variables"][0];
         this.semanticError("Initialization Problem", "The property <code>"+variable.name+"</code> was read on line "+variable.position.line+", but it was possibly not given a value on a previous line. You cannot use a property until it has been initialized. Check to make sure that this variable was declared in all of the branches of your decision.", variable.position.line);
         return true;
-    } else if ( !suppress["Unread variables"] && 
-               report["Unread variables"].length >= 1) {
+    } else if (!suppress["Unread variables"] && report["Unread variables"].length >= 1) {
         var variable = report["Unread variables"][0];
         this.semanticError("Unused Property", "The property <code>"+variable.name+"</code> was set, but was never used after that.", null)
         return true;
-    } else if (report["Overwritten variables"].length >= 1) {
+    } else if (!suppress["Overwritten variables"] && report["Overwritten variables"].length >= 1) {
         var variable = report["Overwritten variables"][0];
         this.semanticError("Overwritten Property", "The property <code>"+variable.name+"</code> was set, but before it could be read it was changed on line "+variable.position.line+". It is unnecessary to change an existing variable's value without reading it first.", variable.position.line)
         return true;
-    } else if (report["Empty iterations"].length >= 1) {
+    } else if (!suppress["Empty iterations"] && report["Empty iterations"].length >= 1) {
         var variable = report["Empty iterations"][0];
         this.semanticError("Iterating over empty list", "The property <code>"+variable.name+"</code> was set as an empty list, and then you attempted to iterate over it on "+variable.position.line+". You should only iterate over non-empty lists.", variable.position.line)
         return true;
-    } else if (report["Non-list iterations"].length >= 1) {
+    } else if (!suppress["Non-list iterations"] && report["Non-list iterations"].length >= 1) {
         var variable = report["Non-list iterations"][0];
         this.semanticError("Iterating over non-list", "The property <code>"+variable.name+"</code> is not a list, but you attempted to iterate over it on "+variable.position.line+". You should only iterate over non-empty lists.", variable.position.line)
         return true;
-    } else if (report["Incompatible types"].length >= 1) {
+    } else if (!suppress["Incompatible types"] && report["Incompatible types"].length >= 1) {
         var variable = report["Incompatible types"][0];
         this.semanticError("Incompatible types", "You attempted to "+variable.operation+" a "+variable.left.type+" and a "+variable.right.type+" on line "+variable.position.line+". But you can't do that with that operator. Make sure both sides of the operator are the right type.", variable.position.line)
         return true;
