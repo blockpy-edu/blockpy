5895de37168bccbcb16320fb7ef16c5969b09714
Austin Cory Bart <acbart@vt.edu>
2017-07-22 00:38:27 -0400

    Doubly defined set_success, add default value for correct parameter

--- skulpt/my_test.py
+++ skulpt/my_test.py
@@ -1,21 +1,2 @@
-class Feedback(Exception):
+def test(aaa, bbb='name'):
     pass
-    
-f = Feedback()
-print(f)
-#Starting this line
-a = 0
-h = 7 #: testing!
-class A():
-    pass
-b = (2
-     +
-     3)
-=======
-#import weather
-
-#print "Forecasts:", weather.get_forecasts('Blacksburg, VA')
-#print "Temperature:", weather.get_temperature('Blacksburg, VA')
-
-for item in range(1, 4):
-    print item
