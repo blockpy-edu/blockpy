528d73ae9b1481e9d3a3659d2cf1a26dac52b248
lukesg08 <lukesg08@vt.edu>
2017-08-30 16:06:17 -0400

    fixes to instructor API     renamed functions in append, filter, histogram,         and context specific groups     fixed bug in numeric_logic_check involving BinOps     fixed tested and fixed bugs in various instructor api functions

--- src/instructor/instructor_histogram.py
+++ src/instructor/instructor_histogram.py
@@ -10,16 +10,16 @@ Feedback: The program should display a histogram.
 
 '''
 def histogram_missing():
-	ast = parse_program()
-	calls = ast.find_all("Call")
-	plotting = False
-	for call in calls:
-		if call.func.attr == "hist" and call.func.value.id == "plt":
-			plotting = True
-			break
-	if plotting == False:
-		explain("The program should display a histogram.")
-	return not plotting
+    ast = parse_program()
+    calls = ast.find_all("Call")
+    plotting = False
+    for call in calls:
+        if call.func.attr == "hist" and call.func.value.id == "plt":
+            plotting = True
+            break
+    if plotting == False:
+        explain("The program should display a histogram.")
+    return not plotting
 '''
 Name: plot_show_missing
 Pattern:
@@ -29,16 +29,16 @@ Missing
 Feedback: The plot must be explicitly shown to appear in the Printer area.
 '''
 def plot_show_missing():
-	ast = parse_program()
-	calls = ast.find_all("Call")
-	plotting = False
-	for call in calls:
-		if call.func.attr == "show" and call.func.value.id == "plt":
-			plotting = True
-			break
-	if plotting == False:
-		explain("The plot must be explicitly shown to appear in the Printer area.")
-	return not plotting
+    ast = parse_program()
+    calls = ast.find_all("Call")
+    plotting = False
+    for call in calls:
+        if call.func.attr == "show" and call.func.value.id == "plt":
+            plotting = True
+            break
+    if plotting == False:
+        explain("The plot must be explicitly shown to appear in the Printer area.")
+    return not plotting
 '''
 Name: histogram_argument_not_list
 Pattern:
@@ -49,18 +49,18 @@ Feedback: Making a histogram requires a list; <argument> is not a list.
 
 '''
 def histogram_argument_not_list():
-	ast = parse_program()
-	calls = ast.find_all("Call")
-	arg_name = ""
-	for call in calls:
-		if call.func.attr == "hist" and call.func.value.id == "plt":
-			arg = call.args[0]
-			if arg != None and not (arg.data_type == "List" or arg.ast_name == "List"):
-				arg_name = arg.id
-				break
-	if arg_name != "":
-		explain("Making a histogram requires a list; %s is not a list." %(arg_name))
-	return arg_name != ""
+    ast = parse_program()
+    calls = ast.find_all("Call")
+    arg_name = ""
+    for call in calls:
+        if call.func.attr == "hist" and call.func.value.id == "plt":
+            arg = call.args[0]
+            if arg != None and not (arg.data_type == "List" or arg.ast_name == "List"):
+                arg_name = arg.id
+                break
+    if arg_name != "":
+        explain("Making a histogram requires a list; %s is not a list." %(arg_name))
+    return arg_name != ""
 '''
 Name: histogram_wrong_list
 Pattern:
@@ -76,32 +76,32 @@ Feedback: The list created in the iteration is not the list being used to create
 '''
 
 def histogram_wrong_list():
-	ast = parse_program()
-	loops = ast.find_all("For")
-	append_targets = []
-	for loop in loops:
-		calls = loop.find_all("Call")
-		for call in calls:
-			if call.func.attr == "append":
-				append_targets.append(call.func.value)
-	all_proper_plot = True
-	#should probably actually check for the location of plt.hist
-	calls = ast.find_all("Call")
-	for call in calls:
-		if call.func.attr == "hist" and call.func.value.id == "plt":
-			arg = call.args[0]
-			proper_plot = False
-			if arg.ast_name == "Name":
-				for name in append_targets:
-					if name.id == arg.id:
-						proper_plot = True
-						break
-				if not proper_plot:
-					all_proper_plot = False
-					break
-			else:
-				all_proper_plot = False
-				break
-	if not all_proper_plot:
-		explain("The list created in the iteration is not the list being used to create the histogram.")
-	return not all_proper_plot
+    ast = parse_program()
+    loops = ast.find_all("For")
+    append_targets = []
+    for loop in loops:
+        calls = loop.find_all("Call")
+        for call in calls:
+            if call.func.attr == "append":
+                append_targets.append(call.func.value)
+    all_proper_plot = True
+    #should probably actually check for the location of plt.hist
+    calls = ast.find_all("Call")
+    for call in calls:
+        if call.func.attr == "hist" and call.func.value.id == "plt":
+            arg = call.args[0]
+            proper_plot = False
+            if arg.ast_name == "Name":
+                for name in append_targets:
+                    if name.id == arg.id:
+                        proper_plot = True
+                        break
+                if not proper_plot:
+                    all_proper_plot = False
+                    break
+            else:
+                all_proper_plot = False
+                break
+    if not all_proper_plot:
+        explain("The list created in the iteration is not the list being used to create the histogram.")
+    return not all_proper_plot
