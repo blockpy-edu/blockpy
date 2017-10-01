ec7474d054b392ebaf9f584c6c6e202b4e6c97b4
Austin Cory Bart <acbart@vt.edu>
2017-10-01 19:06:11 -0400

    Dummy execution for give_feedback

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ Blockly.WorkspaceSvg.prototype = {};
 skulpt = require('./skulpt/dist/skulpt.min.js')
 skulpt_libs = require('./skulpt/dist/skulpt-stdlib.js')
 blockpy = require('./dist/blockpy_node.js')
+var trace = function() {
+    
+};
+trace.removeAll = x => x;
 engine = new blockpy.BlockPyEngine({
     'components': {
         'printer': {
@@ -16,6 +20,14 @@ engine = new blockpy.BlockPyEngine({
         },
         'server': {
             'logEvent': x => x,
+            'markSuccess': x => x,
+        },
+        'feedback': {
+            'isFeedbackVisible': x => true,
+            'presentFeedback': x => console.log(x),
+        },
+        'toolbar': {
+            
         }
     },
     'model': {
@@ -23,16 +35,15 @@ engine = new blockpy.BlockPyEngine({
             'reports': {},
             'suppressions': {},
             'status': v => this['status'] = v,
-            'trace': {
-                'removeAll': x => x,
-            },
+            'trace': trace,
             'step': x => x,
             'last_step': x => x,
             'line_number': x => x,
             'show_trace': x => x,
         },
         'programs': {
-            '__main__': v => 'a = 0\nprint(a)'
+            '__main__': v => 'a = 0\nprint(a)',
+            'give_feedback': v => 'log(student.data)',
         },
         'settings': {
             'disable_timeout': x => false,
